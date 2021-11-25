eb0e671adc16a73ec318cd3bf27eeb74123e6d65
Lei Xu <leixu@leixu-macpro16>
2021-11-25 12:19:19 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SmartIDE' });
+  res.render('index', { title: 'Hello SmartIDE' });
 });
 
 module.exports = router;
